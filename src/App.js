0f77cf9a8233cfb79b1edc5ae0273099b6e77b38
aindrila22 <aindrilabhattacharjee72@gmail.com>
2021-12-02 23:29:48 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Nav from "./components/Nav";
+import PermanentDrawerLeft from "./components/Sidebar";
+import { Container, Grid, Paper } from "@mui/material";
+import BasicCard from "./components/Card";
+
+const styles = {
+  paperContainer: {
+    backgroundImage: `url(${"https://images.unsplash.com/photo-1638398877139-fe9caae20f4b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80"})`,
+    height: "auto",
+
+    display: "flex",
+    width: "100%",
+    backgroundRepeat: "no-repeat",
+  },
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Nav />
+      <Container sx={{ display: "flex" }}>
+        <Paper style={styles.paperContainer}>
+          <PermanentDrawerLeft />{" "}
+          <Grid
+            container
+            spacing={4}
+            sx={{ mt: 10, gap: 2, flex: 1, mb: 3, ml: 3 }}
+          >
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map(
+              (item, index) => {
+                return <BasicCard key={index} />;
+              }
+            )}
+          </Grid>
+        </Paper>
+      </Container>
     </div>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,63 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+
+const drawerWidth = 240;
+
+export default function PermanentDrawerLeft() {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+            mt: 6,
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <List>
+          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {["All mail", "Trash", "Spam"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}
+      >
+        <Toolbar />
+      </Box>
+    </Box>
+  );
+}
